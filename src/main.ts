4bcc3de7a1c2a07a81d084a9f638a0955db45a4b
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-03 17:06:57 +0100

    add Is String Palindrome

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,21 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+function checkPalindrome( palindrome: string){
+   let original: string = "";
+   let reverse: string = "";
+
+   original = palindrome;
+
+   let length1:number = original.length;
+
+   for (let i = length1 - 1; i >= 0; i--){
+    reverse = reverse + original.charAt(i);
+   }
+
+   if( original == reverse)
+        console.log("Entered string is a Palindrome");
+    
+    else
+        console.log("Entered string is not Palindrome");
+        
+}
+
+console.log(checkPalindrome("mum"));
