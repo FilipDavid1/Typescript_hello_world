85b380cdb7810e6f885888eab2fafd634aade4b8
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-03 18:11:23 +0100

    add War of Numbers project

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+let a:number[] =new Array(3223,2,333,44,5);
+
+
+function warOfNumbers(numbers: number[]){
+    let even = 0;
+    let odd = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (i % 2 == 0)
+            even += numbers[i];
+
+        else
+            odd +=  numbers[i];
+
+    }
+
+    if (even > odd)
+        console.log(even - odd);
+
+    else
+        console.log(odd - even);
+}
+
+console.log(warOfNumbers(a));
