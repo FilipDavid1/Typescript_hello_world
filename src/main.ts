ba32e0c26638e69a215d46e580d1501fb3a9c6da
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-03 16:19:39 +0100

    add Equality of 3 numbers project

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+
+function equal(a: number, b: number, c: number){
+    if (a == b && a == c && b == c)
+    return 3;
+
+    else if (a == b || a == c)
+    return 2;
+
+    else 
+    return 0;
+}
+
+console.log(equal(1,2,1));
