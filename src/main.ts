29b423babb4c4d47740dca2a22a0613727aff6f4
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-03 15:38:55 +0100

    add Snail Project

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+
+function totalDistance(stepLenght:number, stepHeight:number, towerHeight:number) {
+    if(stepLenght < 0){
+        return false;
+    }
+    else if(stepHeight < 0){
+        return false;
+    }
+    else if(towerHeight < 0){
+        return false;
+    }
+
+
+    let divisor = stepLenght + stepHeight;
+
+    return (towerHeight / stepHeight) * divisor;
+}
+
+
+console.log(totalDistance(0.2, 0.4, 100));
