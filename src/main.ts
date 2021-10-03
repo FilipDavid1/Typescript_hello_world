1790c04b26755d12ddd79acb345c872c824ff88f
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-03 16:36:05 +0100

    add Slice of Pie project

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+function equalSlices(Slices: number, People: number, SliceForOne: number){
+    let Multiplier: number = People * SliceForOne;
+
+    let x: boolean;
+
+    if(Multiplier <= Slices)
+        x = true;
+
+    else 
+        x = false;
+
+    return x;
+}
+
+console.log(equalSlices(9, 3, 3));
