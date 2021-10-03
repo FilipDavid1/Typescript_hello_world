63138c19e4729d716a7075bc8f7381739a1fb2a0
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-03 16:28:48 +0100

    add Is it a triangle project

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,17 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+function isTriangle(a:number, b:number, c:number){
+    let x: boolean;
+
+    if(a < b && b < c)
+        x = true;
+
+    else if(a == b && b == c)
+        x = true;
+
+    else
+        x = false;
+
+
+    return x;
+}
+
+console.log(isTriangle(22,1,3.4));
