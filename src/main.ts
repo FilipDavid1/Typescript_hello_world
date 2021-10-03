cfe74e12799f8d9d4d532a64b780c0e1e29e4128
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-03 18:01:11 +0100

    add Difference of Max and Min Number project

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+let a:number[] = new Array(32,-50,30);
+
+function differenceMaxMin( numbers: number[]){
+
+    let sortedArray = numbers.sort((n1,n2) => n1 - n2);
+
+    console.log(sortedArray);
+
+    let first: number  = sortedArray[0];
+    let last: number = sortedArray[numbers.length -1];
+
+    return Math.abs(first - last);
+}
+
+console.log(differenceMaxMin(a));
