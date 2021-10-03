6691dcc304b44de6ce5e497fd28567965ed12409
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-03 16:49:22 +0100

    add Multypli by Lenght project

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+
+
+let a:number[] = new Array(1,34,54,120,33);
+
+function multiplyByLenght(arr:number[]){
+    arr.forEach(element => {
+        console.log(element * arr.length);
+    });
+}
+
+
+console.log(multiplyByLenght(a));
