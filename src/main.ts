8780a27619586e09b7fdb365bc3bdd92fa0e02ba
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-04 16:52:40 +0100

    add Check if String Ending Matches Second String project

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+ function checkString(a: string, b: string ){
+     if(a.charAt(a.length -1) == b.charAt(b.length -1))
+        return true;
+    else
+        return false;
+ }
+
+ console.log(checkString("jj", "hhg"));
